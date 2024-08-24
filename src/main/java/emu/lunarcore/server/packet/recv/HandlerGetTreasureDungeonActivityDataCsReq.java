8263beb0e8949b0e159af1b25c31d042f2eaaed1
Kostas214pro <kdoukas2006@gmail.com>
2024-08-24 16:42:48 +0300

    Implemented HandlerGetTreasureDungeonActivityDataCsReq

---
+++ src/main/java/emu/lunarcore/server/packet/recv/HandlerGetTreasureDungeonActivityDataCsReq.java
@@ -0,0 +1,14 @@
+package emu.lunarcore.server.packet.recv;
+
+import emu.lunarcore.server.game.GameSession;
+import emu.lunarcore.server.packet.CmdId;
+import emu.lunarcore.server.packet.Opcodes;
+import emu.lunarcore.server.packet.PacketHandler;
+
+@Opcodes(CmdId.GetTreasureDungeonActivityDataCsReq)
+public class HandlerGetTreasureDungeonActivityDataCsReq extends PacketHandler {
+    @Override
+    public void handle(GameSession session, byte[] data) throws Exception {
+        session.send(CmdId.GetTreasureDungeonActivityDataScRsp);
+    }
+}
